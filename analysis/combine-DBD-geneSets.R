6b85ecd7e99aeff23af64e40ecec3eabfc559e50
twesleyb <tyler.w.bradshaw@duke.edu>
2020-01-25 12:58:49 -0500

    adding Satterstrom data to combined dbd genes

--- analysis/combine-DBD-geneSets.R
+++ analysis/combine-DBD-geneSets.R
@@ -13,9 +13,14 @@ tabsdir <- file.path(root,"tables")
 downdir <- file.path(root,"downloads")
 
 # Load geneSets.
-patterns <- c("SFARI-Gene_genes","SFARI-Gene_animal","DDG2P",
-	      "DisGeneNet_All_Disease_Genes","URMC","DBD-Genes")
-myfiles <- sapply(patterns,function(x) list.files(rdatdir,x,full.names=TRUE))
+datasets <- c("SFARI-Gene_genes",
+	      "SFARI-Gene_animal",
+	      "DDG2P",
+	      "DisGeneNet_All_Disease_Genes",
+	      "URMC",
+	      "DBD-Genes",
+	      "Satterstrom")
+myfiles <- sapply(datasets,function(x) list.files(rdatdir,x,full.names=TRUE))
 geneSets <- lapply(myfiles,readRDS)
 names(geneSets) <- tools::file_path_sans_ext(basename(myfiles))
 
