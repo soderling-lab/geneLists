00c2e1fbcb3cf580c9e64aed57b1ef5fb11e4e94
twesleyb <twesleyb10@gmail.com>
2021-08-29 15:28:16 -0400

    need to write tests!

---
+++ tests/testthat.R
@@ -0,0 +1,6 @@
+#!/usr/bin/env Rscript
+
+library(testthat)
+library(geneLists)
+
+test_check("geneLists")
---
+++ tests/testthat/test-example.R
@@ -0,0 +1,4 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+  expect_equal(5, as.numeric(5))
+})
